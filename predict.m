59f0b391e5208e52853bbbdc5f27b6f272675a13
Elizabeth Morales Muñoz <elizabeth.morales@ucsp.edu.pe>
2021-01-14 17:03:51 -0500

    parte 3

--- predict.m
+++ predict.m
@@ -1,26 +1,36 @@
-function p = predict(theta, X)
-  %PREDICT Predict whether the label is 0 or 1 using learned logistic 
-  %regression parameters theta
-  %   p = PREDICT(theta, X) computes the predictions for X using a 
-  %   threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
-  
-  m = size(X, 1); % Number of training examples
-  
-  % You need to return the following variables correctly
-  p = zeros(m, 1);
-  
-  % ====================== YOUR CODE HERE ======================
-  % Instructions: Complete the following code to make predictions using
-  %               your learned logistic regression parameters. 
-  %               You should set p to a vector of 0's and 1's
-  %
-  % Dimentions:
-  % X     =  m x (n+1)
-  % theta = (n+1) x 1
-  
-  h_x = sigmoid(X*theta);
-  p=(h_x>=0.5);
-  
-  %p = double(sigmoid(X * theta)>=0.5);
-  % =========================================================================
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+%
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
+%
+X = [ones(size(X, 1), 1) X];
+H1 = sigmoid(X * Theta1');
+H1 = [ones(size(H1, 1), 1) H1];
+H2 = sigmoid(H1 * Theta2');
+[val index] = max(H2, [], 2);
+p = index;
+
+
+
+
+% =========================================================================
+
+
 end
