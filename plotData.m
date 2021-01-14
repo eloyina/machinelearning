a3a4114a0f4955834e1e937af4d436a68ca97ed1
Elizabeth Morales Muñoz <elizabeth.morales@ucsp.edu.pe>
2021-01-14 17:01:04 -0500

    parte 2
    función sigmoid

--- plotData.m
+++ plotData.m
@@ -1,21 +1,35 @@
-function plotData(x, y)
-%PLOTDATA Plots the data points x and y into a new figure 
-%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
-%   population and profit.
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Plot the training data into a figure using the 
-%               "figure" and "plot" commands. Set the axes labels using
-%               the "xlabel" and "ylabel" commands. Assume the 
-%               population and revenue data have been passed in
-%               as the x and y arguments of this function.
-%
-% Hint: You can use the 'rx' option with plot to have the markers
-%       appear as red crosses. Furthermore, you can make the
-%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
-
-figure; % open a new figure window
-
-plot(x , y , 'rx' , 'MarkerSize' , 10);
-ylabel('Profit in $10,000s');
-xlabel('Population of City in $10,000s');
+function plotData(X, y)
+  %PLOTDATA Plots the data points X and y into a new figure 
+  %   PLOTDATA(x,y) plots the data points with + for the positive examples
+  %   and o for the negative examples. X is assumed to be a Mx2 matrix.
+  
+  % ====================== YOUR CODE HERE ======================
+  % Instructions: Plot the positive and negative examples on a
+  %               2D plot, using the option 'k+' for the positive
+  %               examples and 'ko' for the negative examples.
+  %
+  
+  %Seperating positive and negative results
+  pos = find(y==1); %index of positive results
+  neg = find(y==0); %index of negative results
+  
+  % Create New Figure
+  figure;
+  
+  %Plotting Positive Results on 
+  %    X_axis: Exam1 Score =  X(pos,1)
+  %    Y_axis: Exam2 Score =  X(pos,2)
+  plot(X(pos,1),X(pos,2),'g+');
+  
+  %To keep above plotted graph as it is.
+  hold on;  
+  
+  %Plotting Negative Results on 
+  %    X_axis: Exam1 Score =  X(neg,1)
+  %    Y_axis: Exam2 Score =  X(neg,2)
+  plot(X(neg,1),X(neg,2),'ro');
+  
+  % =========================================================================
+  
+  hold off;
+end
